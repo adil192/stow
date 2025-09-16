c10921c4e8835f1b3d681c6f2b6f54dbe82a3085
Adil Hanney <adilhanney@disroot.org>
2025-09-16 19:31:02 +0100

    feat: add AbstractCodec and DelegateCodec

--- packages/stow_codecs/lib/stow_codecs.dart
+++ packages/stow_codecs/lib/stow_codecs.dart
@@ -1,5 +1,7 @@
+export 'src/abstract_codec.dart';
 export 'src/bool_to_string_codec.dart';
 export 'src/color_codec.dart';
+export 'src/delegate_codec.dart';
 export 'src/enum_codec.dart';
 export 'src/identity_codec.dart';
 export 'src/int_to_string_codec.dart';
